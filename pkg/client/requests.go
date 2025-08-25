652f765174db1855c5d80c6c6ad16a23e0f859d9
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-08-25 15:03:48 +0700

    [server] add API endpoint for sending messages

---
+++ pkg/client/requests.go
@@ -0,0 +1,8 @@
+package client
+
+type MessagesPOSTRequest struct {
+	// Phone number of the recipient
+	PhoneNumber string `json:"phoneNumber" validate:"required,e164" example:"+15551234567"`
+	// Message text
+	Text string `json:"text" validate:"required,min=1,max=4096" example:"Hello from phone2tg-proxy"`
+}
